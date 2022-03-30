ab4f632234215d03c25761371849355e9591d4b3
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-30 14:25:06 +0430

    Removed local state to save user info to display on user profile page

--- src/layout/UserInfo/index.tsx
+++ src/layout/UserInfo/index.tsx
@@ -1,21 +1,13 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { GitHub } from "react-feather";
 import { storeProps } from "../../store";
-import type { User } from "../../store/users";
 import "./style.scss";
 
 export default function UserInfo() {
   const { username } = useParams();
   const { cachedUsers } = useSelector((state: storeProps) => state.users);
-  const [user, setUser] = useState<User>({});
-
-  useEffect(() => {
-    if (username) {
-      setUser(cachedUsers[username]);
-    }
-  }, [cachedUsers, username]);
+  const user = username && cachedUsers[username];
 
   return (
     <div className="user-info flex-center">
--- src/layout/UserRepos/index.tsx
+++ src/layout/UserRepos/index.tsx
@@ -1,8 +1,6 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { storeProps } from "../../store";
-import type { User } from "../../store/users";
 import { RepositoryBox } from "../../components";
 import "./style.scss";
 
@@ -18,13 +16,7 @@ interface RepoProps {
 export default function UserRepos() {
   const { username } = useParams();
   const { cachedUsers } = useSelector((state: storeProps) => state.users);
-  const [user, setUser] = useState<User>({});
-
-  useEffect(() => {
-    if (username) {
-      setUser(cachedUsers[username]);
-    }
-  }, [cachedUsers, username]);
+  const user = username && cachedUsers[username];
 
   return (
     <div className="user-repos">
